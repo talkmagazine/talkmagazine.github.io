72de65900a04eb5814ca08fcb5840174a3e4a2d7
Eric Hu <ericjhu@gmail.com>
2015-02-03 16:59:30 -0500

    canvas vibez

--- js/app.concat.js
+++ js/app.concat.js
@@ -8834,6 +8834,31 @@ $(function () {
 	var app = {
 		init: function () {
 
+			var el = document.getElementById('c');
+			var ctx = el.getContext('2d');
+			var isDrawing;
+
+			el.width = window.innerWidth;
+			el.height = window.innerHeight;
+
+			document.onmousemove = function(e) {
+			  isDrawing = true;
+			  ctx.lineWidth = 50;
+			  ctx.lineJoin = ctx.lineCap = 'round';
+			  ctx.shadowBlur = 10;
+			  ctx.shadowColor = 'rgb(255, 255, 255)';
+			  ctx.moveTo(e.clientX, e.clientY);
+			};
+			el.onmousemove = function(e) {
+			  if (isDrawing) {
+			    ctx.lineTo(e.clientX, e.clientY);
+			    ctx.stroke();
+			  }
+			};
+			// el.onmouseup = function() {
+			//   isDrawing = false;
+			// };
+
 		}
 	};
 
