77c4c724ba5f807e0a17c300f91c268f9805ee00
Eric Hu <ericjhu@gmail.com>
2015-06-01 14:44:37 -0400

    countdown

--- js/app.concat.js
+++ js/app.concat.js
@@ -8834,31 +8834,52 @@ $(function () {
 	var app = {
 		init: function () {
 
-			var el = document.getElementById('c');
-			var ctx = el.getContext('2d');
-			var isDrawing;
+			this._initCountdown();
+			this._initTwitterAPI();
+			this._initInstagramAPI();
 
-			el.width = window.innerWidth;
-			el.height = window.innerHeight;
+		},
 
-			document.onmousemove = function(e) {
-			  isDrawing = true;
-			  ctx.lineWidth = 50;
-			  ctx.lineJoin = ctx.lineCap = 'round';
-			  // ctx.strokeStyle = 'rgb(255,255,255)';
-			  ctx.shadowBlur = 10;
-			  ctx.shadowColor = 'rgb(255, 255, 255)';
-			  ctx.moveTo(e.clientX, e.clientY);
-			};
-			el.onmousemove = function(e) {
-			  if (isDrawing) {
-			    ctx.lineTo(e.clientX, e.clientY);
-			    ctx.stroke();
-			  }
-			};
-			// el.onmouseup = function() {
-			//   isDrawing = false;
-			// };
+		_initCountdown: function() {
+			
+			// set the date we're counting down to
+			var targetDate = new Date("June 4, 2015 15:00:00").getTime();
+
+			// variables for time units
+			var days, hours, minutes, seconds;
+
+			// get tag element
+			var $countdown = $('.countdown');
+
+			// update the tag with id "countdown" every 1 second
+			setInterval(function () {
+
+				// find the amount of "seconds" between now and target
+				var current_date = new Date().getTime();
+				var seconds_left = (targetDate - current_date) / 1000;
+		
+				// do some time calculations
+				days = parseInt(seconds_left / 86400);
+				seconds_left = seconds_left % 86400;
+		
+				hours = parseInt(seconds_left / 3600);
+				seconds_left = seconds_left % 3600;
+		
+				minutes = parseInt(seconds_left / 60);
+				seconds = parseInt(seconds_left % 60);
+		
+				// format countdown string + set tag value
+				$countdown.html(days + ' days and ' + hours + ' hours and<br>' + minutes + ' minutes and ' + seconds + ' seconds' );
+				
+			}, 0);
+
+		},
+
+		_initTwitterAPI: function () {
+
+		},
+
+		_initInstagramAPI: function(){
 
 		}
 	};
